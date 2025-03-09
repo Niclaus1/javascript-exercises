19ae94217a2b451fd677e4e444247e1c1e14e153
Niclaus1 <rendonniclaus111@gmail.com>
2025-03-09 09:56:31 +0800

    Created reverse for-loop in exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+// split the text
+// create for-loop for reverse text
+const reverseString = function(word) {
+    let string = ""
+    let words = word.split("");
+    for (let index = words.length - 1; index > -1 ; index--) {
+        string += words[index];
+    }
+    return string;
 
 };
 
